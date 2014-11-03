22239b75417f1ab0ae4fa02a77eedbf7078bee2d
MHaroonBaig <haroon.prog@gmail.com>
2014-11-03 18:42:23 +0500

    Added contants

--- Practice.playground/section-1.swift
+++ Practice.playground/section-1.swift
@@ -5,7 +5,8 @@ import UIKit
 
 // This files contains all the basic syntax including operators, initialisations and things like that
 
-// Variables -- self explainatory.
+// -------- Variables -- self explainatory ----------
+// --------------------------------------------------
 
 // Explicit declaration
 var someString:String
@@ -24,5 +25,22 @@ someBool = true
 // Printing
 println(someString)
 print("This line would be printed but without an endLine character at the end")
+println("This is the Integer \(someInt), and this is a float \(someFloat)")
+
+
+// --------- Constants -------------
+// ---------------------------------
+
+// They must be initialised at the sane time when they are declared
+let someConstant:String = "Hello, this is a constant and it's value cannot be changed"
+let constantInt:Int = 34
+print(constantInt)
+// Their value cannot be changed throughout the code.
+
+
+
+
+
+
 
 
